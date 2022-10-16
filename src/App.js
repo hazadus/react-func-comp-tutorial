8b9cfd1eb0bc7b73ee87490ba193d64c5db0de81
hazadus <hazadus7@gmail.com>
2022-10-16 15:26:10 +0300

    All the stuff moved up to App.js.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,81 @@ import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Footer from "./components/Footer/Footer";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {useState, useEffect} from "react";
 
 function App() {
-  return (
-        <Router>
-            <Header/>
-            <Routes>
-                <Route path='/'
-                       element={
-                        <>
-                            <Content/>
-                        </>
-                       }>
-                </Route>
-            </Routes>
-            <Footer/>
-        </Router>
-  );
-}
+    const defaultCards = [
+    {
+        id: 1,
+        title: 'Mayhem Devil',
+        set: 'WAR',
+        image: 'MayhemDevil.jpg',
+        deck: '',
+    },
+    {
+        id: 2,
+        title: 'Priest of Forgotten Gods',
+        set: 'RAV',
+        image: 'PriestOfTheForgottenGods.jpg',
+        deck: '',
+    },
+    {
+        id: 3,
+        title: 'Cauldron Familiar',
+        set: 'ELD',
+        image: 'CauldronFamiliar.jpg',
+        deck: '',
+    },
+    {
+        id: 4,
+        title: 'Witch\'s Oven',
+        set: 'ELD',
+        image: 'WitchsOven.jpg',
+        deck: '',
+    },
+    {
+        id: 5,
+        title: 'Woe Strider',
+        set: 'ELD',
+        image: 'WoeStrider.jpg',
+    },
+]
+
+    // Returns a "stateful value" and a function to update it.
+    // 'setCards' is function reference and is used whenever we need to change 'cards'.
+    // When 'cards' changed through the 'setCards' call, Collection component is re-rendered.
+    const [cards, setCards] = useState(JSON.parse(localStorage.getItem('cardsList')) || defaultCards)
+    const [selectedDeck, setDeck] = useState(JSON.parse(localStorage.getItem('selectedDeck')) || 'Deck3')
+
+    // What function to execute ... when what changes ...
+    // [] - 'effect' will be executed only when component first loads
+    useEffect(() => {
+        // When the 'cards' changing, we're updating local storage
+        localStorage.setItem('cardsList', JSON.stringify(cards))
+    }, [cards]);
+    useEffect(() => {
+        localStorage.setItem('selectedDeck', JSON.stringify(selectedDeck))
+    }, [selectedDeck]);
+
+    // 'Prop drilling' - pass props from parent to child components
+      return (
+            <Router>
+                <Header/>
+                <Routes>
+                    <Route path='/'
+                           element={
+                            <>
+                                <Content cards={cards}
+                                         setCards={setCards}
+                                         selectedDeck={selectedDeck}
+                                         setDeck={setDeck}/>
+                            </>
+                           }>
+                    </Route>
+                </Routes>
+                <Footer/>
+            </Router>
+      );
+    }
 
 export default App;
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -3,67 +3,12 @@ import {useState, useEffect} from "react";
 import Collection from "../Collection/Collection";
 import './Content.css'
 
-const Content = () => {
-    const defaultCards = [
-    {
-        id: 1,
-        title: 'Mayhem Devil',
-        set: 'WAR',
-        image: 'MayhemDevil.jpg',
-        deck: '',
-    },
-    {
-        id: 2,
-        title: 'Priest of Forgotten Gods',
-        set: 'RAV',
-        image: 'PriestOfTheForgottenGods.jpg',
-        deck: '',
-    },
-    {
-        id: 3,
-        title: 'Cauldron Familiar',
-        set: 'ELD',
-        image: 'CauldronFamiliar.jpg',
-        deck: '',
-    },
-    {
-        id: 4,
-        title: 'Witch\'s Oven',
-        set: 'ELD',
-        image: 'WitchsOven.jpg',
-        deck: '',
-    },
-    {
-        id: 5,
-        title: 'Woe Strider',
-        set: 'ELD',
-        image: 'WoeStrider.jpg',
-    },
-]
-
-    // Returns a "stateful value" and a function to update it.
-    // 'setCards' is function reference and is used whenever we need to change 'cards'.
-    // When 'cards' changed through the 'setCards' call, Collection component is re-rendered.
-    // const [cards, setCards] = useState(defaultCards)
-    const [cards, setCards] = useState(JSON.parse(localStorage.getItem('cardsList')) || defaultCards)
-    // const [selectedDeck, setDeck] = useState('Deck3')
-    const [selectedDeck, setDeck] = useState(JSON.parse(localStorage.getItem('selectedDeck')) || 'Deck3')
-
-    // What function to execute ... when what changes ...
-    // [] - 'effect' will be executed only when component first loads
-    useEffect(() => {
-        // When the 'cards' changing, we're updating local storage
-        localStorage.setItem('cardsList', JSON.stringify(cards))
-    }, [cards]);
-    useEffect(() => {
-        localStorage.setItem('selectedDeck', JSON.stringify(selectedDeck))
-    }, [selectedDeck]);
+const Content = ({cards, setCards, selectedDeck, setDeck}) => {
 
     function handleDeckSelectionChange(event) {
         setDeck(event.target.value)
     }
 
-    // 'Prop drilling' - pass props from parent to child components
     return (
         <main>
             <h1>Cards Collection</h1>
